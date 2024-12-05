faf83d321b681cd3fb3971a223487b3fd6d869d8
dcbarros <dcbarros1@gmail.com>
2024-12-05 19:15:43 -0300

    feat: novas alterações para a rota de autenticação

--- src/main/java/com/upload/files/upload_files/controller/AuthController.java
+++ src/main/java/com/upload/files/upload_files/controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.upload.files.upload_files.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.upload.files.upload_files.service.AuthService;
@@ -8,6 +11,12 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping
+    public String authenticate(Authentication auth){
+        return authService.authenticate(auth);
+    }
 }
--- src/main/java/com/upload/files/upload_files/service/AuthService.java
+++ src/main/java/com/upload/files/upload_files/service/AuthService.java
@@ -1,8 +1,16 @@
 package com.upload.files.upload_files.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.security.core.Authentication;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
-    
+    private final JwtService jwtService;
+
+    public String authenticate(Authentication authentication){
+        return jwtService.generateToken(authentication);
+    }
 }
