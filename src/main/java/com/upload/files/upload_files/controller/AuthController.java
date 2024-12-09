f19a1989385342dc85f8d46165edbc43410e47dd
dcbarros <dcbarros1@gmail.com>
2024-12-09 19:08:26 -0300

    feat: Adiciona autenticação a página

--- src/main/java/com/upload/files/upload_files/controller/AuthController.java
+++ src/main/java/com/upload/files/upload_files/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.upload.files.upload_files.controller;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.upload.files.upload_files.model.dto.AuthResponse;
 import com.upload.files.upload_files.security.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
@@ -17,7 +19,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("authenticate")
-    public String authenticate(
+    public AuthResponse authenticate(
         Authentication authentication) {
       return authService.authenticate(authentication);
     }
--- src/main/java/com/upload/files/upload_files/security/service/AuthService.java
+++ src/main/java/com/upload/files/upload_files/security/service/AuthService.java
@@ -1,6 +1,9 @@
 package com.upload.files.upload_files.security.service;
 
 import org.springframework.stereotype.Service;
+
+import com.upload.files.upload_files.model.dto.AuthResponse;
+
 import org.springframework.security.core.Authentication;
 
 import lombok.RequiredArgsConstructor;
@@ -10,7 +13,8 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
     private final JwtService jwtService;
 
-    public String authenticate(Authentication authentication) {
-        return jwtService.generateToken(authentication);
-      }
+    public AuthResponse authenticate(Authentication authentication) {
+        AuthResponse token = new AuthResponse(jwtService.generateToken(authentication));
+        return token;
+    }
 }
